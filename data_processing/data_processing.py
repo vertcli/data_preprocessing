a8b0b293d50551620a80934b6fba9a5360a3fb3c
Albert Climent Bigas <vert.cli@gmail.com>
2022-11-18 20:53:42 +0100

    Final commit

--- data_processing/data_processing.py
+++ data_processing/data_processing.py
@@ -1,1 +1,18 @@
 """Main module."""
+
+import pandas as pd
+from typing import Sequence, Dict, Optional, Tuple
+
+def one_hot_encoder(df: pd.DataFrame, column_list: Sequence[str] = None) -> pd.DataFrame:
+  """_summary_
+
+  Args:
+      df (pd.DataFrame): Input dataframe
+      column_list (Sequence[str], optional): List of all columns to convert to dummy. Defaults to None.
+
+  Returns:
+      pd.DataFrame: _description_
+  """  
+  #ffgfdd
+  # ['hola']
+  return df
